27dacf2b43a460d7654f6a36771a431f0f181760
Jande Royalty <janderoyalty@gmail.com>
2023-08-21 21:15:01 -0500

    Transactions details show up in users because of changes made in the serializers

--- wemes/serializers.py
+++ wemes/serializers.py
@@ -2,10 +2,17 @@ from rest_framework import serializers
 from .models import *
 
 class UserSerializer(serializers.ModelSerializer):
+    transactions = serializers.SerializerMethodField()  # Use SerializerMethodField
+
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'phone_num', 'last_four', 'email', 'admin', 'is_active', 'transactions']
 
+    def get_transactions(self, user):  # Custom method to get transactions
+        transactions = Transaction.objects.filter(customer=user)  # Adjust the filter condition based on your model relationships
+        transaction_serializer = TransactionSerializer(transactions, many=True)
+        return transaction_serializer.data
+    
 class TransactionSerializer(serializers.ModelSerializer):
     # parent_lookup_kwargs = {
     #     'user': 'user',
