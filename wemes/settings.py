acfa1970d54696a9cfccc4c074e0d48e9dcddb24
Jande Royalty <janderoyalty@gmail.com>
2023-08-22 20:50:01 -0500

    Changes ALLOWED_HOSTS

--- wemes/settings.py
+++ wemes/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False	
 
-ALLOWED_HOSTS = ['127.0.0.1', 'wemes-be.herokuapp.com', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
