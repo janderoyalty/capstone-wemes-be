a03dab62af7ecb8edbe14b396f9b8fa374dc857e
Jande Royalty <janderoyalty@gmail.com>
2023-08-22 20:56:52 -0500

    Changes ALLOWED_HOSTS

--- wemes/settings.py
+++ wemes/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False	
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'wemes-be.onrender.com', 'localhost', '0.0.0.0']
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
