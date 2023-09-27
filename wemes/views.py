7675b1e2598f09d4811329fc980cdd0544f00af6
Jande Royalty <janderoyalty@gmail.com>
2023-09-27 05:10:53 -0500

    Changes 'permission_classes' to 'permissions.IsAuthenticatedOrReadOnly'

--- wemes/views.py
+++ wemes/views.py
@@ -7,29 +7,29 @@ from .models import *
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class ColorViewSet(viewsets.ModelViewSet):
     queryset = Color.objects.all()
     serializer_class =ColorSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 # class QRCodeViewSet(viewsets.ModelViewSet):
 #     queryset = QRCode.objects.all()
 #     serializer_class = QRCodeSerializer
-#     permission_classes = [permissions.IsAdminUser]
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
