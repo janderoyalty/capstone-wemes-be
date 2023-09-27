a6351ef44b043d3fa012e9fb1da4e1c7d65f0937
Jande Royalty <janderoyalty@gmail.com>
2023-09-27 05:07:24 -0500

    Changes 'permission_classes' to 'permissions.IsAdminUser'

--- wemes/views.py
+++ wemes/views.py
@@ -7,29 +7,29 @@ from .models import *
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
 class ColorViewSet(viewsets.ModelViewSet):
     queryset = Color.objects.all()
     serializer_class =ColorSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
 # class QRCodeViewSet(viewsets.ModelViewSet):
 #     queryset = QRCode.objects.all()
 #     serializer_class = QRCodeSerializer
-#     permission_classes = [permissions.IsAuthenticated]
+#     permission_classes = [permissions.IsAdminUser]
